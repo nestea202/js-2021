459c766c6338c93084962dff792c133f1bdc6626
Holteiu Alin-Marian <alin_holteiu@yahoo.com>
2021-01-08 20:52:56 +0200

    lecture 3

---
+++ car/car.js
@@ -0,0 +1,25 @@
+let dacia = {
+   make: "Dacia",
+   model: 1310,
+   color: "verde",
+   year: 1978,
+   mileage: 525737,
+   maxSpeed: 180,
+   paint: function (color) {
+      this.color = color
+   }
+}
+let ferrari = {
+    make: "Dino",
+    model: 1310,
+    color: "rosu",
+    year: 1978,
+    mileage: 525737,
+    maxSpeed: 180,
+    paint: function (color) {
+       this.color = color
+    }
+ }
+ dacia.paint ("galben")
+ console.log(dacia)
+ console.log(ferrari)
