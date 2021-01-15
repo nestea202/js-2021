b12cf6c50522bd6784ef68a50afd8feb5a585370
Holteiu Alin-Marian <alin_holteiu@yahoo.com>
2021-01-15 20:47:55 +0200

    lecture 5

---
+++ bubbles/bubbles.js
@@ -0,0 +1,18 @@
+var numerOfBubbles = 100
+for (let i = 0; i < numerOfBubbles; i++) {
+    newBubble()
+    
+}
+function newBubble() {
+    let bubble = document.createElement("div")
+    bubble.classList.add("bubble")
+    let x = randomNumber(100)
+    let delay = randomNumber(5000)
+    bubble.style.left =  x + "vw" 
+    bubble.style.animationDelay = delay + "ms"
+    document.querySelector("body").appendChild(bubble)
+
+}
+function randomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
