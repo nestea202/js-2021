c6f4b3de21707e32167829bacf298d5fcb581fa6
Holteiu Alin-Marian <alin_holteiu@yahoo.com>
2021-01-18 20:47:03 +0200

    lecture 6

--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -8,8 +8,16 @@ function newBubble() {
     bubble.classList.add("bubble")
     let x = randomNumber(100)
     let delay = randomNumber(5000)
-    bubble.style.left =  x + "vw" 
-    bubble.style.animationDelay = delay + "ms"
+    let red = randomNumber(255)
+    let green = randomNumber(255)
+    let blue = randomNumber(255)
+    bubble.style.left =  `${x}vw`
+    bubble.style.animationDelay = `${delay}ms`
+    let size = randomNumber(3) + 1
+    //bubble.style.borderColor = "rgb("+ red + "," + green + "," + blue + ")"//
+    bubble.style.borderColor = `rgb(${red},${green},${blue})`
+    bubble.style.height = bubble.style.width = `${size}em`
+     
     document.querySelector("body").appendChild(bubble)
 
 }
